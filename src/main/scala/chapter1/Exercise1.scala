d71b48d198d639c760cbdc253958d69cace71ec3
lesia.karchevska <lesia.kachevska@ihsmarkit.com>
2019-08-29 17:46:10 +0300

    minor fix

--- src/main/scala/chapter1/Exercise1.scala
+++ src/main/scala/chapter1/Exercise1.scala
@@ -1,7 +1,7 @@
 package chapter1
 
 
-private object Exercise1 {
+object Exercise1 {
 
   //exercise 1 Implement compose method with the given signature
   def compose[A, B, C] (g: B => C, f: A => B): A => C =
